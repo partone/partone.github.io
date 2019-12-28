005bb475f99aa97d314586f0c1166e66da74aae2
Eric Parton <partonemp@gmail.com>
2019-12-28 13:52:20 -0600

    Tabique hosting

--- Tabique/js/home.js
+++ Tabique/js/home.js
@@ -57,4 +57,19 @@ $( document ).ready(function() {
     function removeSelectedClasses() {
       $(".headerBarElement").removeClass("headerBarElementSelected");
     }
+
+    //Initial header check
+    var scroll = $(window).scrollTop();
+    console.log($("#contentNosotros").offset().top + ($("#contentProyectos").height() / 2));
+    console.log(scroll)
+    if (scroll < $("#contentLanding").height() / 2) {
+      removeSelectedClasses();
+      $("#inicioButton").addClass("headerBarElementSelected");
+    } else if(scroll > $("#contentNosotros").offset().top + ($("#contentProyectos").height() / 2)) {
+      removeSelectedClasses();
+      $("#proyectosButton").addClass("headerBarElementSelected");
+    } else if(scroll > $("#contentLanding").offset().top + ($("#contentNosotros").height() / 2)) {
+      removeSelectedClasses();
+      $("#quienesSomosButton").addClass("headerBarElementSelected");
+    }
 });
