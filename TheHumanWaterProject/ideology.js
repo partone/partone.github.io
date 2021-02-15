81b9aa33b4da7744100d51ad7ed142a254da6545
Eric <partonemp@gmail.com>
2021-02-15 13:41:30 -0800

    hwp

--- TheHumanWaterProject/ideology.js
+++ TheHumanWaterProject/ideology.js
@@ -27,13 +27,23 @@ const ideologies = [
 
     ["Shinto", `>Then the eighty Deities spoke to the hare, saying: "What thou shouldest do is to bathe in the sea-*water* here, and lie on the slope of a high mountain exposed to the blowing of the wind." So the hare followed the instructions of the eighty Deities, and lay down.\n\n` + `The Kojiki, Section XXI — The White Hare of Inaba\n\nThe Kojiki has many references to water, one of our favourites occurs when the [Deities of Deity Master-of-the-Great-Land stumbled upon a hare at Cape Keta](https://www.sacred-texts.com/shi/kj/kj028.htm).  They confirm the existence of water as a substance that is related to the sea, likely what we have coined as "seawater" in modern times.`],
 
-    ["Falun Gong", `> \n\n` + `\n\n`],
-    ["Judaism", `> \n\n` + `\n\n`],
+    ["Falun Gong", `>
+    Student: Is there anything we are required to do after finishing the exercises, such as
+    rubbing the face?
+    Teacher: After doing the exercises we don’t worry about cold *water* and those kinds
+    of things, nor do we need to rub the face or hands.\n\n` + `Falun Gong, 7th Translation Edition\n\nThrough [Hongzhi's teachings](https://falundafa.org/eng/eng/pdf/flg_en.pdf), we can understand that cold water is not needed in the practice of opening the human body’s meridians and acupuncture points, thus indicating the existence of water through its lack of necessity in this practice.\n\nWhile there are few references to literal water in Falun Gong, [Li Hongzhi was pictured with, what appear to be, bottles of **water** at a Falun Gong conference in New York in 2017](TheHumanWaterProject/images/HongzhiWithWater.jpg).`],
+
+    ["Judaism", `> יִזַּל־מַ֨יִם֙ מִדָּ֣לְיָ֔ו וְזַרְע֖וֹ בְּמַ֣יִם רַבִּ֑ים וְיָרֹ֤ם מֵֽאֲגַג֙ מַלְכּ֔וֹ וְתִנַּשֵּׂ֖א מַלְכֻתֽוֹ\n\n` + `Bamidbar - Numbers, Chapter 24, 7\n\nWe are fortunate to have not one, not two, but three, references to water in [this passage](https://www.jewishvirtuallibrary.org/bamidbar-numbers-chapter-24) though Balaam's description the bountiful settlement that G-d had helped the Israelites construct.  It also indicates the belief that trees need water to grow, a commonly held belief in modern times.`],
 
     ["Confucianism", `> First, of the five elements - The first is *water*; the second is fire; the third, wood; the fourth, metal; and the fifth, earth. (The nature of) *water* is to soak and descend\n\n` + `Passage from part five of The Shu Ching\n\nFrom the [first of the Five Classics](https://www.sacred-texts.com/cfu/sbe03/index.htm) comes this world-defining passage that positions water's role as one of the five elements.  Hereafter, water is referenced by Confucius, but also in various symbolic ways to represent flexibility but also force`],
 
-    ["Caodaism", `> \n\n` + `\n\n`],
+    ["Caodaism", `>
+    Cu, show him your steps.
+    Children, watch Me step.
+    Hieu, bring Me some *water*.\n\n` + `Cao Đài Tiên Ông Đại Bồ Tát Ma Ha Tát, Teaching the Great Way to the Southern Quarter\n\nIn this small excerpt from [The Collection of Divine Messages](https://anyflip.com/uisu/jyaf/basic), The Jade Emperor requests an otherwise unmentioned character, Hieu, to bring Him water.  Thus confirming that the existence of water does not conflict with the Caodaist faith.`],
+
     ["Baháʼí Faith", `> \n\n` + `\n\n`],
+
     ["Jainism", `> \n\n` + `\n\n`],
     ["Cheondoism", `> \n\n` + `\n\n`],
     ["Hoahaoism", `> \n\n` + `\n\n`],
@@ -94,8 +104,9 @@ const ideologies = [
     ["Vegetarianism", `> \n\n` + `\n\n`],
     ["Pescetarianism", `> \n\n` + `\n\n`],
     ["Neo-feudalism", `> \n\n` + `\n\n`],
-    ["Feudalism", `> \n\n` + `\n\n`],
-    ["Fuedalism", `> \n\n` + `\n\n`],
+
+    ["Feudalism", `Feudalism (not to be confused with neo-feudalism) is an ideology with no defining text or doctrine, and thus proving the existence of water within this ideology poses a large challenge.  However, through manuscripts and records from Medieval Europe, The Human Water Project has established with certainty that water did play a role in the feudal system.\n\nThe Gotha Manuscript describes and illustrates [Holy Roman Emperor Fredrick I's death from excessive exposure to **water**](TheHumanWaterProject/images/FredrickWithWater.jpg) (drowining) on June 10th, 1190.  The existence of water also affected the common classes as illustrated in a French manuscript dating from around 1270 that [portrays two fishermen riding a boat atop **water** than contains a fish](TheHumanWaterProject/images/PlebsWithWater.jpg).  It is important to clarify that these two examples are not exhaustive; there are innumerous manuscripts from Medieval Europe depicting water in different situations across all levels of the feudal system.\n\nGiven that water existed in both the highest and lowest strata of the feudal system, we can infer that the middle classes also lived with its presence.  This evidence can be compounded into confirmation that belief in water does not intefere with, and is practically confirmed by, feudalist belief.`],
+
     ["Bonapartism", `> \n\n` + `\n\n`],
     ["Bismarkianism", `> \n\n` + `\n\n`],
     ["Neo-liberalism", `> \n\n` + `\n\n`],
@@ -113,13 +124,18 @@ const ideologies = [
     ["Classical liberal radicalism", `> \n\n` + `\n\n`],
     ["Nationalism", `> \n\n` + `\n\n`],
     ["Anti-imperialism", `> \n\n` + `\n\n`],
+
     ["Anti-Zionism", `Due to the **The Fact Society**'s strict policy of non-interventionism in the Israeli–Palestinian conflict (1965-present), we are unable to provide evidence for the existence of water within this ideology at the current time.\n\nBe sure to check back when the conflict resolves!`],
+
     ["Zionism", `Due to the **The Fact Society**'s strict policy of non-interventionism in the Israeli–Palestinian conflict (1965-present), we are unable to provide evidence for the existence of water within this ideology at the current time.\n\nBe sure to check back when the conflict resolves!`],
+
     ["Pan-Africanism", `> \n\n` + `\n\n`],
     ["National populism", `> \n\n` + `\n\n`],
     ["Progressivism", `> \n\n` + `\n\n`],
     ["Freemasonry", `> \n\n` + `\n\n`],
-    ["Gandhism", `> \n\n` + `\n\n`],
+
+    ["Gandhism", `> The spinning wheel is as much a necessity of Indian life as air and *water*\n\n` + `Mahatma Gandhi\n\nMahatma Gandhi has several quotes attributed to him that make reference to water.  We chose [this piece of wisdom](https://www.mkgandhi.org/epigrams/c.htm) in particular as it shows the importance as water, ranking it as high as air and spinning wheels.  Additionally, [an image of Gandhi drinking **water**](TheHumanWaterProject/images/GandhiWithWater.jpg) entered circulation after being published in an Indian newspaper on January 26th, 1948.`],
+
     ["Mormonism", `> \n\n` + `\n\n`],
     ["Communitarianism", `> \n\n` + `\n\n`],
     ["Anti-socialism", `> \n\n` + `\n\n`],
