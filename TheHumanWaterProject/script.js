75eef0caa9efdffdbfa088cc259cb1ba42938705
Eric <partonemp@gmail.com>
2021-02-14 21:24:02 -0800

    HumanWaterProject

---
+++ TheHumanWaterProject/script.js
@@ -0,0 +1,54 @@
+// Populate dropdown
+const ideologiesSorted = ideologies.sort((a, b) => a[0].localeCompare(b[0]));
+for (i = 0; i < ideologiesSorted.length; i++) {
+    if(ideologiesSorted[i][1] != `> \n\n` + `\n\n`) {
+        $('#beliefsList').append('<div class="belief" style="display:none">' + ideologiesSorted[i][0] + '</div>');
+    }
+}
+
+function toSecond() {
+    $("#firstView").fadeOut("fast");
+    $("#secondView").fadeIn("fast");
+    $("#beliefInput").focus();
+}
+
+function thirdToSecond() {
+    document.getElementById("beliefInput").value = "";
+    $(".belief").hide();
+    $("#thirdView").fadeOut("fast");
+    $("#secondView").fadeIn("fast");
+    $("#beliefInput").focus();
+}
+
+$(".belief").click(function(e) {
+    $("#beliefTitle").text($(e.target).text());
+    $("#proofOfWater").html(markdown(ideologies.find(x => x[0] === $(e.target).text())[1]))
+    $("#secondView").fadeOut("fast");
+    $("#thirdView").fadeIn("fast");
+});
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("beliefInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = document.getElementsByClassName("belief");
+    displayed = 0;
+    $(".belief").hide();
+    if(filter === "") {
+        return;
+    }
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            displayed++;
+            a[i].style.display = "";
+            if(displayed === 5) {
+                displayed = 0;
+                return;
+            }
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
